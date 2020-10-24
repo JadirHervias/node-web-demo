099076ff55dfab3d91958cbf3e4043eecf5465c4
Genaro <genaro.salas@tecsup.edu.pe>
2020-10-23 23:20:49 -0500

    vistas listas

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const router = require('./router');
 
 const port = 4000;
 
+app.use('templates/data_lib', express.static(path.join(__dirname, 'data_lib')));
+
 http.createServer(router).listen(port, () => {
   console.log(`Server listening on ${port}`);
 });
